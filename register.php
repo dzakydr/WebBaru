97368162fe665720cade7c67aafef30ba323f642
dzakydr <dzaky.14116041@student.itera.ac.id>
2018-11-20 22:07:01 +0700

    Update register.php

--- register.php
+++ register.php
@@ -56,75 +56,36 @@
             </div>
         </div>
 
-        <?php
-        if($_POST['register']){
-          $conn = new mysqli("localhost", "root", "", "bootstrap");
-          $nama   = $conn->real_escape_string($_POST['nama']);
-          $email  = $conn->real_escape_string($_POST['email']);
-          $user   = $conn->real_escape_string($_POST['username']);
-          $pass   = $conn->real_escape_string($_POST['password']);
-          $pass2  = $conn->real_escape_string($_POST['password2']);
-          $tgl    = date("Y-m-d");
-          if(strlen($pass) >= 5){
-            if($pass == $pass2){
-              $password = md5($pass);
-              $insert = $conn->query("INSERT INTO user(tgl_daftar, nama, username, password) VALUES('$tgl', '$nama', '$user', '$password')");
-              if($insert){
-                echo '<div class="alert alert-success">Register berhasil, silahkan <a href="login.php">Login</a>.</div>';
-              }else{
-                echo '<div class="alert alert-danger">Gagal melakukan Register.</div>';
-              }
-            }else{
-              echo '<div class="alert alert-danger">Konfirmasi password tidak sesuai.</div>';
-            }
-          }else{
-            echo '<div class="alert alert-danger">Password minimal 5 karakter.</div>';
-          }
-        }
-        ?>
-
-        <form class="form-horizontal" method="post">
-          <div class="form-group">
-            <label class="col-md-4 control-label">Nama Lengkap</label>
-              <input type="text" name="nama" class="form-control" placeholder="Nama Lengkap" >
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">Email</label>
-              <input type="email" name="email" class="form-control" placeholder="email@domain.com">
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">Username</label>
-              <input type="text" name="username" class="form-control" placeholder="username" >
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">Password</label>
-              <input type="password" name="password" class="form-control" placeholder="password">
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">Ulangi Password</label>
-              <input type="password" name="password2" class="form-control" placeholder="ulangi password" >
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">&nbsp;</label>
-              <input type="submit" name="register" class="btn btn-primary" value="Register">
-          </div>
-
-          <div class="form-group">
-            <label class="col-md-4 control-label">&nbsp;</label>
-              Sudah punya akun? <a href="login.php">Login</a>
+        <div class="outter-form-login">
+            <div class="logo-login">
+                <em class="glyphicon glyphicon-user"></em>
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-    <hr>
-  </div>
+            <form action="send_register.php" class="inner-login" method="post" enctype="multipart/form">
+            <h3 class="text-center title-login">Registrasi</h3>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="nickname" placeholder="Nama">
+                </div>
 
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" name="repassword" placeholder="Re-Password">
+                </div>
+
+                <input type="submit" class="btn btn-block btn-custom-green" value="REGISTER" />
+                
+                <div class="text-center forget">
+                    <p>Back To <a href="./login.php">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
 </section>
 <?php
     /* handle error */
