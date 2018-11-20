ec312fdbb48c6c7a9994b9d8419165be4b5f9d94
dzakydr <dzaky.14116041@student.itera.ac.id>
2018-11-20 18:37:35 +0700

    Update config.php

--- on-member/config.php
+++ on-member/config.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 session_start();
 
-$conn = new mysqli("localhost", "root", "", "dbmateri");
+$conn = new mysqli("localhost", "root", "", "bootstrap");
 if ($conn->connect_errno) {
     echo die("Failed to connect to MySQL: " . $conn->connect_error);
 }
