bd567bc699eddea6a70f6b2cf95b784ecad40f5c
AjipBimo <45142055+AjipBimo@users.noreply.github.com>
2018-11-22 06:31:35 +0700

    Update download.php

--- on-member/download.php
+++ on-member/download.php
@@ -6,7 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>SMK Tentang Rindu</title>
-  <img src="gambar/Logo.png" width="30px" class="rounded-circle img-thumbnail mr-2" >
   <link href="css/bootstrap.min.css" rel="stylesheet">
 
   <style type="text/css">
@@ -42,9 +41,6 @@
 
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-        <a class="navbar-brand" href="../Home.html">
-           SMK Tentang Rindu
-          </a>
           <li><a href="index.php">Home</a></li>
           <li class="active"><a href="download.php">Download</a></li>
         </ul>
